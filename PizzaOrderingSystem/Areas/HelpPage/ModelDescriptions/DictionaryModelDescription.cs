86a20f661421c8acd5f6273803e2c9d2a24e700e
nileshsathe1988 <nileshsathe.nilesh@gmail.com>
2021-06-23 02:36:22 +0530

    sfds

---
+++ PizzaOrderingSystem/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PizzaOrderingSystem.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ PizzaOrderingSystem/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PizzaOrderingSystem.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
