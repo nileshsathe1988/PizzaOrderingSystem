3109bc479d5c1af577e8a8681adf67931c0f1b0b
nileshsathe1988 <nileshsathe.nilesh@gmail.com>
2021-06-23 02:44:32 +0530

    jjhgjh

---
+++ PizzaOrderingSystem/Controllers/PizzaController.cs
@@ -0,0 +1,61 @@
+﻿using PizaaOrderingLib;
+using DataModel.Interface;
+using DataModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PizzaOrderingSystem.Controllers
+{
+    [RoutePrefix("api/pizza")]
+    public class PizzaController : ApiController
+    {
+        IAbstractPizza _pizza = new PizzaFactory();
+        PizzaClient client;
+
+        public PizzaController()
+        {
+            this.client = new PizzaClient(_pizza);
+        }
+
+        // GET api/values
+        [Route("GetPizzaCustSize")]
+        [HttpGet]
+        public List<CustSize> GetPizzaCustSize()
+        {
+            return this.client.getPizzaCustSize();
+        }
+
+        [Route("GetSauses")]
+        [HttpGet]
+        public List<Sauses> GetSauses()
+        {
+            return this.client.getSauses();
+        }
+
+        [Route("GetToppings")]
+        [HttpGet]
+        public List<Toppings> GetToppings()
+        {
+            return this.client.getToppings();
+        }
+
+        [Route("GetAllOrders")]
+        [HttpGet]
+        public List<Orders> GetAllOrders()
+        {
+            return this.client.getOrders();
+        }
+
+        [Route("SaveOrder")]
+        [HttpPost]
+        // POST api/values
+        public Orders Post([FromBody] Orders orders)
+        {
+            return this.client.addOrders(orders);
+        }
+    }
+}
